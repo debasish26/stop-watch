443a3f5d64784a7c0eac4fb38e6b401090e901a6
debasish024h <73976256+debasish26@users.noreply.github.com>
2021-07-21 13:23:00 +0530

    Add files via upload

---
+++ stopewatch.py
@@ -0,0 +1,14 @@
+print("Stop Watch")
+print("Enter the number of seconds")
+import time
+a=int(input())
+while a!=0:
+  print(str(a)+    "seconds remaining")
+  time.sleep(1)
+  a=a-1
+print("the time is up")
+time.sleep(1)
+print("stop watch by debasish")
+
+  
+  
